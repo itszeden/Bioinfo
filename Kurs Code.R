df6a671d7bf3180219788c13f79bff1f115eea3c
itszeden <104253119+itszeden@users.noreply.github.com>
2022-04-26 16:26:46 +0200

    Add files via upload

---
+++ Kurs Code.R
@@ -0,0 +1,378 @@
+0.15*19.75
+0.50*20
+a <- 1
+b <- 1
+c <- 1
+a
+b
+c
+d
+print(a)
+print(b)
+ls
+ls()
+ls()
+(-b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+(-b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+( -b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+(-b + sqrt (b^2 - 4*a*c) ) / ( 2*a )
+(-b + sqrt(b^2 - 4 * a * c) ) / ( 2*a )
+(-b + sqrt(b^2 - 4*a*c)) / ( 2*a )
+( -b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+(-b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+sqrt(a)
+x <- 25
+sqrt(25)
+a * b
+a+b
+a*x
+(-b + sqrt (x))
+x^2
+sqrt(b - 4*a)
+sqrt(4*a)
+sqrt(b - (4*a*c))
+sqrt (b^2)
+x / a
+4*a*c
+b + sqrt(a + b)
+sqrt(b^2 )
+(-b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+sqrt(b^2 - 4)
+sqrt(x^2 - a)
+sqrtx^2 - 4
+sqrt(x^2 - 4)
+sqrt(x^2 - 4*a*c)
+(-b + sqrt(x^2 - 4*a*c) ) / ( 2*a )
+(-b + sqrt(b^2 - 4*a*c) ) / ( 2*a )
+ls()
+log(8)
+log(x)
+exp(x)
+exp(a)
+log(2.718)
+log(exp(1))
+log(exp(1))
+log(exp(a))
+help(log)
+args(log)
+args(sqrt)
+log(8,base=2)
+log(x=8,base=2)
+log(8,2)
+2^3
+2^5
+help(+)
+help+
+Co2
+pi
+inf
+inf
+Inf
+solution_1 <- (-b + sqrt(x^2 - 4*a*c) ) / ( 2*a ) )
+a <- a
+a <- 1
+b <- 1
+c <- 1
+x <- 25
+solution_1 <- (-b + sqrt(x^2 - 4*a*c) ) / ( 2*a ) )
+solution_1 <- (-b + sqrt(x^2 - 4*a*c))
+solution_2 <- solution_1 <- (-b - sqrt(x^2 - 4*a*c))
+(-b + sqrt(x^2 - 4*a*c)) / ( 2*a ) )
+(-b + sqrt(x^2 - 4*a*c)) / ( 2*a ))
+(-b + sqrt(x^2 - 4*a*c)) / ( 2*a ) )
+(-b + sqrt(x^2 - 4*a*c) ) / ( 2*a ) )
+( -b + sqrt(x^2 - 4*a*c) ) / ( 2*a ) )
+( -b + sqrt( x^2 - 4*a*c) ) / ( 2*a ) )
+class(a)
+class(ls)
+la()
+ls()
+install.packages("dslabs")
+libraries("dslab")
+library(dslabs)
+library(dslabs)
+data("murders")
+class(murders)
+str(murders)
+head(murders)
+class(a)
+library(dslabs)
+data("murders")
+class(murders)
+str(murders)
+head(murders)
+murders$population
+murders$region
+murders$state
+str(murders)
+names(murders)
+murders$population
+pop <- murders$population
+lenght(po)
+length(pop)
+n <- 1000
+x <- (1,n)
+n <- 1000
+x <- seq(1,n)
+sum(x)
+library(dslabs)
+data(murders)
+class(murders)
+str(murders)
+head(murders)
+murders$state
+pop
+stat <- murders$sates
+stat <- murders$states
+stat <- murders$state
+lenght (stat)
+length(stat)
+class(pop)
+class(stat)
+m <- 3==3
+cöas(m)
+class(m)
+class(murders$region)
+levels(murders$region)
+head(murders)
+str(murders)
+1 == 2
+a <- 2
+b <- -1
+c <- -4
+(-b + sqrt(4*a*c))
+log4(1024)
+log4 (1024)
+log(1024,4)
+library(dslabs)
+data(movielens)
+levels
+levels(movielens)
+length(movielens)
+head(movielens)
+structure(movielens)
+str(movielens)
+class(movielens$title)
+class(movielens$genres)
+gen <- movielens$genres
+nlevels(gen)
+codes <- c(380,124,818)
+country <- c("italy","canada","egypt")
+codes <- c(italy=380,canada=124,egypt=818)
+codes
+codes<- (380,124,818)
+codes <- (380,124,818)
+codes <- (380,124,818)
+codes <- c(380,124,818)
+country <- c("italy","canada","egypt")
+names(codes) <- country
+codes
+seq(1,10)
+seq(1,10,2)
+seq (1,100,5)
+1:10
+codes
+codes[2]
+codes(3)
+codes[3]
+codes
+codes[2]
+codes[c(1,2)]
+codes[c(2,3)]
+codes [2canada]
+codes [c("egypt, "canada)]
+codes [c("egypt","canada)]
+cd
+codes <- c(380,124,818)
+country <- c("italy","canada","egypt")
+names(codes) <- country
+codes
+codes [c(1,2)]
+codes ["canada"]
+c <- c(1,"canada",3)
+c
+x <- 1:5
+y <- as.character (x)
+class(x)
+y
+class (y)
+x <- 1:5
+y <- as.character (x)
+y
+class (y)
+as.numeric(y)
+class(y)
+class(x)
+library(dslabs)
+data(murders)
+sort(murders$total)
+x <- c(31,4,15,92,65)
+x
+sort(x)
+index <- order(x)
+x [index]
+order (x)
+murders$state [1.10]
+murders$state [1:10]
+murders$abb [1:10]
+##sorting states after murder numbers
+index <- order(murders$total)
+murders$abb[index]
+max(murders$total)
+min(murders$total)
+max(murder$total)
+max(murders$total)
+i.max <- which.max(murders$total)
+i.max
+murders$states [i.max]
+max(murders$total)
+i_max <- which.max(murders$total)
+i_max
+murders$state [i_max]
+min(murders$total)
+a <- which.min(murders$total)
+a
+murders$state [a]
+x <- c(31,4,15,92,65)
+x
+rank(x)
+states <- murders$states
+states <- murders$state
+ranks <- rank(murders$population)
+my_df <- data.frame(states = states, ranks = ranks)
+View(my_df)
+states <- murders$state
+ranks <- rank(murders$population)
+ind <- order(murders$population)
+my_dt <- data.frame( states = states, ranks = ranks)
+View(my_dt)
+View(my_df)
+View(my_df)
+View(my_df)
+library(dslabs)
+data(na_example)
+str(na_example)
+mean (na_example)
+ind <- in.na(na_example)
+sum(ind)
+x <- c(1,2,3)
+library(dslabs)
+data(heights)
+options(digits = 3)
+mean(heights)
+heigts
+heights
+mean(height)
+ind <- is.na(heights)
+mean(heights)
+mean(heights[!ind])
+heights
+mean(heights, height)
+mean(height)
+heights
+library(dslabs)
+data(heights)
+options(digits = 3)
+str(heights)
+mean(height)
+mean(heights$height)
+mean(heights$height)
+ind <- heights$height > 68.3
+ind
+height$sex [ind]
+heights$sex[ind]
+index<- which(heights$height > 68.3)
+index
+mean(heights$height)
+ind <- sum(heights$height > 68.3)
+ind
+ind <- sum((heights$height > 68.3)& == "Female")
+a <- heights$height > 68.3
+b <- heights$sex == "Female"
+ind <- sum(a & b)
+ind
+ind <- sum(b)
+ind
+sum(heights$sex)
+c <- heights$sex == "Male"
+ind <- sum(c)
+ind
+b <- heights$sex == "Female"
+b
+ind <- sum(b)
+ind
+mean(heights$height)
+a <- heights$sex == "Male"
+b <- heights$sex == "Female"
+ind <- sum(a & b)
+ind
+ind <- sum(a)
+ind
+ind <- sum(b)
+ind
+mean(heights$sex == "Female")
+min(heights$height)
+index <- match(min(heights$height),index)
+index
+inde <- match(min(heights$height))
+ind <- match(min(heights$height),index)
+ind
+ind <- match(min)
+match(50, heigts$heights)
+match(50, heights$height)
+match(1032, heights$sex)
+heights$sex[1032]
+max(heights$height)
+x <- 50:83
+x
+x <- 11:20
+x
+x <- 51:83
+x
+library(dslabs)
+data(heights)
+options(digits = 3)
+sum(50:83)
+x <- 50:82
+sum(!x %in% heights$height)
+ht_cm <- mutate(heights, heights2=height*2.54 )
+ht_cm <- mutate(heights, heights2=height*2.54)
+ht_cm <- mutate(heights, heights2=height*2.54)
+library(dplyr)
+ht_cm <- mutate(heights, heights2=height*2.54)
+ht_cm(18)
+ht_cm[18]
+heights$heights2[18]
+heights$heights2(18)
+library(dplyr)
+heights2 <- mutate(heights, ht_cm=height*2.54)
+head(heights2)
+heights$htcm[18]
+height$ht_cm(18)
+heights$ht_cm(18)
+heights$ht_cm[18]
+heights$height(3)
+heights$height(3)
+heights$height[3]
+heights$ht_cm[3]
+heights[18]
+heights2 <- mutate(heights, ht_cm=height*2.54)
+heights2$ht_cm[18]
+mean(heights2$ht_cm)
+females <- filter(heights2, sex == "female")
+sum(females)
+females <- filter(heights, sex = "Female")
+females <- filter(heights, sex == "Female")
+length(females)
+nlevels(females)
+head(females)
+str(females)
+mean(females$height)
+library(dslabs)
+data(olive)
+head(olive)
+plot(olive$palmitic, olive$palmitoleic)
+hist(olive$eicosenoic)
+boxplot(pamitic~region, data = olive)
+boxplot(palmitic~region, data = olive)
+savehistory("E:/Uni/bioinfo/Kurs Code.R")
