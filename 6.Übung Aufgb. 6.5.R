f8e34a6edc902410923df8403c99ee2a29bacd90
itszeden <104253119+itszeden@users.noreply.github.com>
2022-06-15 16:37:20 +0200

    Add files via upload

---
+++ 6.Übung Aufgb. 6.5.R
@@ -0,0 +1,18 @@
+n <- 10
+p_K <- 0.5
+p_Z <- 0.5
+
+
+Münzenwurf <- sample(c("Kopf","Zahl"), n,replace = T, prob = c(p_K, p_Z))
+Münzenwurf
+
+s_Kopf <- nchar(Münzenwurf["Kopf"])
+s_Kopf
+
+for (i in 1:(n+2)){
+  
+}
+
+# Laplace: n+2 und s+1
+
+
